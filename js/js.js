686ff5c8ca7bda512db7ad5bf802c5a98aabaecb
hsoubecommerce <hsoubecommerce@gmail.com>
2023-08-10 15:07:03 +0200

    transparent addtion

--- js/js.js
+++ js/js.js
@@ -1,16 +1,20 @@
 $(window).scroll(function () {
-    if ($(this).scrollTop() >= 400) {
-        $("#navbar").addClass("noTransparrent");
-    } else {
-        $("#navbar").removeClass("noTransparrent");
-    }
-    
+  if ($(this).scrollTop() >= 400) {
+    $("#navbar").addClass("noTransparent");
+  } else {
+    $("#navbar").removeClass("noTransparent");
+  }
 });
+//================================
+
 $(document).ready(function () {
-    var wow = new WOW({
-      mobile: false,
-    });
-    wow.init();
+  var wow = new WOW({
   });
- 
+  wow.init();
+});
+//preloader event ------------------------------------//
+// will first fade out the loading animation
+$("#loading-animation").fadeOut();
+// will fade out the whole DIV that covers the website.
+$("#preloader").delay(600).fadeOut("slow");
 
