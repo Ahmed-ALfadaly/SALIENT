36e272c1d59200f5d00fe83c806f5dc3ab272d4e
hsoubecommerce <hsoubecommerce@gmail.com>
2023-08-20 18:35:56 +0200

    last touch

--- js/js.js
+++ js/js.js
@@ -18,3 +18,22 @@ $("#loading-animation").fadeOut();
 // will fade out the whole DIV that covers the website.
 $("#preloader").delay(600).fadeOut("slow");
 
+const anims = document.querySelectorAll('.animated')
+const options={ 
+  root:null,
+  rootMargin:'-200px 0px 200px 0px '
+}
+const observer = new IntersectionObserver ((entries) =>{
+  {
+   entries.forEach(el => {
+    el.target.classList.toggle('slide', el.isIntersecting)
+    if(el.isIntersecting){
+      observer.unobserve(el.target)
+    }
+   })
+   } options
+})
+
+anims.forEach(anim => {
+  observer.observe(anim)
+})
